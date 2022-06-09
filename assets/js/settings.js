39843db35be5f57f6853cbab675468cc96d46db9
andergmartins <anderson@publishpress.com>
2022-06-09 14:33:18 -0300

    Partial implementation for fixing the issue

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,10 +1,11 @@
 (function ($) {
+    const INVALID_CLASS = 'invalid';
+
     $(document).ready(function () {
         init();
     });
 
-    function toggleCustomDateBox()
-    {
+    function toggleCustomDateBox() {
         $(this).siblings('.pe-custom-date-container').hide();
 
         if ($(this).val() === 'custom') {
@@ -12,7 +13,57 @@
         }
     }
 
+    function validateRequiredField() {
+        $(this).removeClass(INVALID_CLASS);
+
+        if ($(this).val().trim() === '') {
+            $(this).addClass(INVALID_CLASS);
+        }
+    }
+
+    function cleanUpValidationFromField() {
+        $(this).removeClass(INVALID_CLASS);
+    }
+
+    function showErrorNotice()
+    {
+        hideErrorNotice();
+
+        const $notice = $('<div>');
+        $notice.addClass('pe-notice');
+        $notice.addClass('error');
+        $notice.html('AHa');
+
+        $('.postexpirator-nav-tab-wrapper').after($notice);
+    }
+
+    function hideErrorNotice()
+    {
+        $('.pe-notice.error').remove();
+    }
+
+    function onSubmit(e) {
+        validateRequiredField.bind(document.getElementById('expired-default-date-format'))();
+        validateRequiredField.bind(document.getElementById('expired-default-time-format'))();
+        validateRequiredField.bind(document.getElementById('expired-custom-expiration-date'))();
+
+        if ($('#expirationdate_save_options').find('.invalid').length > 0) {
+            showErrorNotice();
+
+            e.preventDefault();
+            return false;
+        }
+
+        hideErrorNotice();
+    }
+
     function init() {
         $('.pe-custom-date-toggle').on('change', toggleCustomDateBox);
+
+        $('#expired-default-date-format').on('blur', validateRequiredField);
+        $('#expired-default-time-format').on('blur', validateRequiredField);
+        $('#expired-custom-expiration-date').on('blur', validateRequiredField);
+
+        $('#expirationdate_save_options').submit(onSubmit);
     }
 })(jQuery, config);
